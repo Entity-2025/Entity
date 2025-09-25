d4c1ef5f1cc32436cb17cb35b551d3da0613e8b6
karmagate-x <fauzi5323@hotmail.com>
2025-09-26 02:54:21 +0700

    UPDATE ENDPOINT

--- src/components/livetest/LiveTest.jsx
+++ src/components/livetest/LiveTest.jsx
@@ -67,11 +67,11 @@ export default function LiveTest() {
 		}
 	}, []);
 
-	const apiEndpoint = "https://entitygate.com/api/fortest/entity";
+	const apiEndpoint = "https://entitycore.net/api/fortest/entity";
 
 	const endpoint = useMemo(() => {
 		const key = shortKey || "x";
-		return `https://entitygate.com/api/fortest/${key}`;
+		return `https://entitycore.net/api/fortest/${key}`;
 	}, [shortKey]);
 
 	async function testEndpoint() {
